3ed4947c58fa0062bb7011dad07b2e7600ba6b6c
nutsil <silpagui.sa@gmail.com>
2020-09-24 20:02:42 -0300

    rename class

--- src/App.js
+++ src/App.js
@@ -6,29 +6,29 @@ function App() {
   return (
     <div>
       <div className="top">
-        <p className="title">not a fake card</p>
+        <p className="highlight-text">not a fake card</p>
       </div>
 
       <div className="card">
         <div className="topcard">
-          <p className="title">International identification</p>
+          <p className="highlight-text">International identification</p>
         </div>
         <div className="photo-id">
           <img src={photo} alt="mabel" />
         </div>
         <div className="information">
           <p className="caption">Given name:</p>
-          <p className="data title">Mabel</p>
+          <p className="data highlight-text">Mabel</p>
           <p className="caption">Surname:</p>
-          <p className="data title">Pines</p>
+          <p className="data highlight-text">Pines</p>
           <p className="caption">Number ID:</p>
-          <p className="data title">1234567</p>
+          <p className="data highlight-text">1234567</p>
           <p className="caption">Nacionality:</p>
-          <p className="data title">United States</p>
+          <p className="data highlight-text">United States</p>
           <p className="caption">Date of Birth:</p>
-          <p className="data title">23 / 32 / 1708</p>
+          <p className="data highlight-text">23 / 32 / 1708</p>
           <p className="caption">Residence:</p>
-          <p className="data title">Gravity Falls</p>
+          <p className="data highlight-text">Gravity Falls</p>
         </div>
       </div>
     </div>
