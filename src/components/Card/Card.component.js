b18ccc812894077dc78778152cc0989e64777500
nutsil <silpagui.sa@gmail.com>
2020-09-25 12:49:15 -0300

    Implement CardInformation on Card component

--- src/components/Card/Card.component.js
+++ src/components/Card/Card.component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Card.styles.scss";
 import CardPic from "../CarPic/CardPic.component";
+import CardInformation from "../CardInformation/CardInformation.component";
 
 function Card() {
   return (
@@ -9,20 +10,7 @@ function Card() {
         <p className="highlight-text">International identification</p>
       </div>
       <CardPic />
-      <div className="information">
-        <p className="caption">Given name:</p>
-        <p className="data highlight-text">Mabel</p>
-        <p className="caption">Surname:</p>
-        <p className="data highlight-text">Pines</p>
-        <p className="caption">Number ID:</p>
-        <p className="data highlight-text">1234567</p>
-        <p className="caption">Nacionality:</p>
-        <p className="data highlight-text">United States</p>
-        <p className="caption">Date of Birth:</p>
-        <p className="data highlight-text">23 / 32 / 1708</p>
-        <p className="caption">Residence:</p>
-        <p className="data highlight-text">Gravity Falls</p>
-      </div>
+      <CardInformation />
     </div>
   );
 }
