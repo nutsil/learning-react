341fdeab5ac5c4541bc05de6e6cf19da54b524f0
nutsil <silpagui.sa@gmail.com>
2020-09-25 12:58:41 -0300

    Move styles from Card to CardInformation styles

--- src/components/CardInformation/CardInformation.component.js
+++ src/components/CardInformation/CardInformation.component.js
@@ -1,20 +1,21 @@
 import React from "react";
+import "./CardInformation.styles.scss";
 
 function CardInformation() {
   return (
     <div className="information">
       <p className="caption">Given name:</p>
-      <p className="data highlight-text">Mabel</p>
+      <p className="data">Mabel</p>
       <p className="caption">Surname:</p>
-      <p className="data highlight-text">Pines</p>
+      <p className="data">Pines</p>
       <p className="caption">Number ID:</p>
-      <p className="data highlight-text">1234567</p>
+      <p className="data">1234567</p>
       <p className="caption">Nacionality:</p>
-      <p className="data highlight-text">United States</p>
+      <p className="data">United States</p>
       <p className="caption">Date of Birth:</p>
-      <p className="data highlight-text">23 / 32 / 1708</p>
+      <p className="data">23 / 32 / 1708</p>
       <p className="caption">Residence:</p>
-      <p className="data highlight-text">Gravity Falls</p>
+      <p className="data">Gravity Falls</p>
     </div>
   );
 }
