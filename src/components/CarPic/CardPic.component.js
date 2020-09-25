f25a6ce508b531baf48464f2b0b7dd086c7d67a9
nutsil <silpagui.sa@gmail.com>
2020-09-25 12:19:39 -0300

    Implement html elements on CardPic Components

---
+++ src/components/CarPic/CardPic.component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import photo from "../../assets/images/mabel.png";
+
+function CardPic() {
+  return (
+    <div className="photo-id">
+      <img src={photo} alt="mabel" />
+    </div>
+  );
+}
+
+export default CardPic;
