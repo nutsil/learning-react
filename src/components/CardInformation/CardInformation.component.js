2218ace91a84bfcfaaab66eab4ee8681cf2a6f1d
nutsil <silpagui.sa@gmail.com>
2020-09-25 12:46:44 -0300

    Implements html elements on CardInformation component

---
+++ src/components/CardInformation/CardInformation.component.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function CardInformation() {
+  return (
+    <div className="information">
+      <p className="caption">Given name:</p>
+      <p className="data highlight-text">Mabel</p>
+      <p className="caption">Surname:</p>
+      <p className="data highlight-text">Pines</p>
+      <p className="caption">Number ID:</p>
+      <p className="data highlight-text">1234567</p>
+      <p className="caption">Nacionality:</p>
+      <p className="data highlight-text">United States</p>
+      <p className="caption">Date of Birth:</p>
+      <p className="data highlight-text">23 / 32 / 1708</p>
+      <p className="caption">Residence:</p>
+      <p className="data highlight-text">Gravity Falls</p>
+    </div>
+  );
+}
+
+export default CardInformation;
