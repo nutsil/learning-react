f175a50f3e63399ee2c061916729edb675aac0fa
nutsil <silpagui.sa@gmail.com>
2020-09-25 11:58:32 -0300

    Add html on Card component

---
+++ src/components/Card/Card.component.js
@@ -0,0 +1,31 @@
+import React from "react";
+import photo from "../../assets/images/mabel.png";
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="topcard">
+        <p className="highlight-text">International identification</p>
+      </div>
+      <div className="photo-id">
+        <img src={photo} alt="mabel" />
+      </div>
+      <div className="information">
+        <p className="caption">Given name:</p>
+        <p className="data highlight-text">Mabel</p>
+        <p className="caption">Surname:</p>
+        <p className="data highlight-text">Pines</p>
+        <p className="caption">Number ID:</p>
+        <p className="data highlight-text">1234567</p>
+        <p className="caption">Nacionality:</p>
+        <p className="data highlight-text">United States</p>
+        <p className="caption">Date of Birth:</p>
+        <p className="data highlight-text">23 / 32 / 1708</p>
+        <p className="caption">Residence:</p>
+        <p className="data highlight-text">Gravity Falls</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
